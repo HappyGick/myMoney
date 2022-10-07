6d49790d28cd231af8331214f0fe3e89c0dd4dad
HappyGick <egarciayustiz@gmail.com>
2022-10-07 00:27:39 -0400

    nice reactivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { increment, decrement } from './features/counter/counterSlice';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
 
   return (
     <div className="App">
@@ -17,9 +20,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => dispatch(increment())}>
           count is {count}
         </button>
+        <button onClick={() => dispatch(decrement())}>decrement</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
