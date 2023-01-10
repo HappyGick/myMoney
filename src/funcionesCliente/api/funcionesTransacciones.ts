74d63481174dbec1505c10cd921ec1864381a22c
HappyGick <egarciayustiz@gmail.com>
2023-01-10 03:00:53 -0400

    ahora es opcional que las transacciones de los prestamos sean excluidas de la lista

--- src/funcionesCliente/api/funcionesTransacciones.ts
+++ src/funcionesCliente/api/funcionesTransacciones.ts
@@ -6,15 +6,19 @@ import { Etiqueta } from '../clases/transacciones/etiqueta';
 import { Transaccion } from '../clases/transacciones/transaccion';
 import constantes from './constantes';
 
-export function obtenerTransacciones(): Transaccion[] {
+export function obtenerTransacciones(excluirPrestamos: boolean = true): Transaccion[] {
     const txs = useAppSelector((state) => state.transacciones);
     const cuentas = useAppSelector((state) =>  state.cuentas);
     let arrayTxs: Transaccion[] = [];
     for (let i in txs) {
         const protoTx = txs[i];
         const protoCuenta = cuentas[protoTx.cuenta];
-        if (protoTx.etiquetaPrimaria !== constantes.etiquetaPrestamoOtorgado.nombre && protoTx.etiquetaPrimaria !== constantes.etiquetaPrestamoSolicitado.nombre) {
-            const cuenta = new Cuenta(protoCuenta.numCuenta, constantes.bancos[protoCuenta.banco], protoCuenta.tipo, protoCuenta.saldo, protoCuenta.id);
+        if (
+            (protoTx.etiquetaPrimaria === constantes.etiquetaPrestamoOtorgado.nombre ||
+            protoTx.etiquetaPrimaria === constantes.etiquetaPrestamoSolicitado.nombre) &&
+            excluirPrestamos
+        ) continue;
+        const cuenta = new Cuenta(protoCuenta.numCuenta, constantes.bancos[protoCuenta.banco], protoCuenta.tipo, protoCuenta.saldo, protoCuenta.id);
             const etiqueta = new Etiqueta(
                 protoTx.etiquetaPrimaria,
                 ""
@@ -28,7 +32,6 @@ export function obtenerTransacciones(): Transaccion[] {
                 [],
                 protoTx.id
             ))
-        }
     }
     return arrayTxs;
 }
