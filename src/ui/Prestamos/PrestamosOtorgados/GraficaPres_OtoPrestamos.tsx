076bd80237138bbc7bcb555c1f508750d2411ff0
DanielBortot <daniel.bortot@gmail.com>
2023-01-11 01:03:56 -0400

    Graficos y errores

--- src/ui/Prestamos/PrestamosOtorgados/GraficaPres_OtoPrestamos.tsx
+++ src/ui/Prestamos/PrestamosOtorgados/GraficaPres_OtoPrestamos.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom";
 import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
+import { obtenerPrestamosOtorgados } from "../../../funcionesCliente/api/funcionesPrestamos";
 
 export const GraficaPresOtoPrestamo = ()=>{
     const data = [{name: 'Eduardo', uv: 50},{name: 'Alfredo', uv:40}, {name: 'Chayane', uv: 30}, {name: 'Daniel', uv: 30}];
@@ -11,17 +12,36 @@ export const GraficaPresOtoPrestamo = ()=>{
         nav('/menu_OtoPres');
     };
 
+    const prestamos=obtenerPrestamosOtorgados();
+    let presOto=[];
+
+    let n='';
+    for (let i=0;i<prestamos.length;i++){
+        let bool=true;
+        n=prestamos[i].deudor.nombre;
+        for (let item of presOto){
+            if (item.name == n){
+                item.cantidad += 1;
+                bool=false;
+                break
+            }
+        }
+        if (bool){
+            presOto.push({name:n, cantidad:1});
+        }
+    }
+
     return (
         <>
             <div className="GrafContainer">
             <h1>Prestamos Otorgados</h1>
             <h2>Estadisticas de contactos por num prestamos</h2>
-            <BarChart width={850} height={500} data={data}>
+            <BarChart width={850} height={500} data={presOto}>
                 <XAxis dataKey="name" stroke="#8884d8" />
                 <YAxis />
                 <Tooltip />
                 <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                <Bar dataKey="uv" fill="#8884d8" barSize={30} />
+                <Bar dataKey="cantidad" fill="#8884d8" barSize={30} />
             </BarChart>
             </div>
             <div className="botones">
--- src/ui/Prestamos/PrestamosSolicitados/GraficaPres_SolPrestamos.tsx
+++ src/ui/Prestamos/PrestamosSolicitados/GraficaPres_SolPrestamos.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom";
 import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
+import { obtenerPrestamosSolicitados } from "../../../funcionesCliente/api/funcionesPrestamos";
 
 export const GraficaPresSolPrestamo = ()=>{
     const data = [{name: 'Pedro', uv: 4.5},{name: 'Daniel', uv: 3}, {name: 'Eduardo', uv: 2}, {name: 'Chayane', uv: 1}];
@@ -11,17 +12,36 @@ export const GraficaPresSolPrestamo = ()=>{
         nav('/menu_SolPres');
     };
 
+    const prestamos=obtenerPrestamosSolicitados();
+    let presSol=[];
+
+    let n='';
+    for (let i=0;i<prestamos.length;i++){
+        let bool=true;
+        n=prestamos[i].acreedor.nombre;
+        for (let item of presSol){
+            if (item.name == n){
+                item.cantidad += 1;
+                bool=false;
+                break
+            }
+        }
+        if (bool){
+            presSol.push({name:n, cantidad:1});
+        }
+    }
+
     return (
         <>
             <div className="GrafContainer">
             <h1>Prestamos Solicitados</h1>
-            <h2>Estadistica de contactos por valoracion</h2>
-            <BarChart width={850} height={500} data={data}>
+            <h2>Estadistica de contactos por num de prestamos </h2>
+            <BarChart width={850} height={500} data={presSol}>
                 <XAxis dataKey="name" stroke="#8884d8" />
                 <YAxis />
                 <Tooltip />
                 <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                <Bar dataKey="uv" fill="#8884d8" barSize={30} />
+                <Bar dataKey="cantidad" fill="#8884d8" barSize={30} />
             </BarChart>
             </div>
             <div className="botones">
