4095c8fdb2f52cd457140ae2d708626226a3f762
C102002 <alfnicolasfff@hotmail.com>
2023-01-11 01:16:52 -0400

    grafica transacciones mas comunes medio hecha y consulta con mayor mov mejor

--- src/ui/Transacciones/GraficaTransRelevante.tsx
+++ src/ui/Transacciones/GraficaTransRelevante.tsx
@@ -1,10 +1,37 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom";
 import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid, TooltipProps } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid, TooltipProps, Legend } from 'recharts';
 import { obtenerTransacciones } from "../../funcionesCliente/api/funcionesTransacciones";
 import { Transaccion } from "../../funcionesCliente/clases/transacciones/transaccion";
 import { NameType, ValueType } from "recharts/types/component/DefaultTooltipContent";
+import { DateTime } from "luxon";
+import { Cuenta } from "../../funcionesCliente/clases/cuentas/cuenta";
+import { Etiqueta } from "../../funcionesCliente/clases/transacciones/etiqueta";
+import { v4 } from "uuid";
+//intente acomodarlo a traves de otra clase pero no me funciono
+export class TransaccionMod {
+  private _name:string;
+  private _id: string;
+  private _valor: number;
+  private _fecha: string;
+  private _cuenta: Cuenta;
+  private _descripcion: string;
+  private _etiquetaPrimaria: Etiqueta;
+  private _etiquetasSecundarias: Etiqueta[];
+
+constructor(valor: number, cuenta: Cuenta, fecha: string, descripcion: string, etiquetaPrimaria: Etiqueta, etiquetasSecudarias: Etiqueta[], name:string,id?: string) {
+  this._valor = valor;
+  this._cuenta = cuenta;
+  this._descripcion = descripcion;
+  this._etiquetaPrimaria = etiquetaPrimaria;
+  this._etiquetasSecundarias = etiquetasSecudarias;
+  this._fecha = fecha;
+  this._name=name;
+  this._id = id ?? v4();
+  
+}
+}
 
 export const GraficaRelvTrans = ()=>{
     const data = [{name: 'Comida', uv: 100000},{name: 'Salidas', uv: 50000}, {name: 'Estudios', uv: 35000}, {name: 'Vivienda', uv: 10000}];
@@ -14,8 +41,30 @@ export const GraficaRelvTrans = ()=>{
         nav('/transacciones');
     };
 
+    function getransmod(trans:Transaccion[])
+    { let solicitados: TransaccionMod[] = [];
+      let a:TransaccionMod
+      for(let i=0;i<=trans.length;i++)
+      {
+        solicitados.push(
+          a=new TransaccionMod(
+              trans[i].valor,
+              trans[i].cuenta,
+              trans[i].fecha,
+              trans[i].descripcion,
+              trans[i].etiquetaPrimaria,
+              trans[i].etiquetasSecundarias,
+              trans[i].id,
+              'T'.concat('',i.toString())
+          )
+      );
+      return(solicitados)
+      }
+    }
+
+
     const transacciones=obtenerTransacciones(true);
-    
+        
     function ordenarPorBurbujaPositivo(arrayDesordenado: Transaccion[]): Transaccion[] {
         arrayDesordenado.sort((a,b)=>{
             if(a.valor<b.valor){return(1);}
@@ -77,7 +126,7 @@ export const GraficaRelvTrans = ()=>{
       };
 
     const transacciones2=ordenarPorBurbujaAbPositivo(transacciones)
-
+    const transacciones3=getransmod(transacciones2)
     return (
         <>
             <div className="GrafContainer">
@@ -86,6 +135,7 @@ export const GraficaRelvTrans = ()=>{
                 <XAxis dataKey="id" stroke="#8884d8" />
                 <YAxis />
                 <Tooltip content={<CustomTooltip/>} wrapperStyle={{ width: 200, backgroundColor: '#13AED4' }}/>
+                <Legend width={100} wrapperStyle={{ top: 40, right: 20, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
                 <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
                 <Bar dataKey="valor" fill="#8884d8" barSize={30} />
                 
