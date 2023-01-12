95d1090a1e26cabc12b7be1a25864e4062dff4af
C102002 <alfnicolasfff@hotmail.com>
2023-01-12 11:30:26 -0400

    error en la redaccion mensaje mas de 15 cuentas

--- src/ui/MensajeErrores/ErrorMensajeCuentaExeso.tsx
+++ src/ui/MensajeErrores/ErrorMensajeCuentaExeso.tsx
@@ -5,7 +5,7 @@ export const ErrorMensajeCuentasExeso = ()=>{
     return(
         <ErrorLista
             url="/cuentas"
-            mensaje="Usted tiene mas de 15 cuentas"
+            mensaje="Usted tiene 15 cuentas"
             mensaje2="Recuerde que no puede tener mas de 15 cuentas por usuario"
             NombreBoton="Volver al menu de cuentas"
         />
