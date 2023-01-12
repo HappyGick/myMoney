a1c4518df575a28aca9756f42a6b1335d41d0047
Fussa <hualong.chiang@gmail.com>
2023-01-11 23:33:25 -0400

    a

--- src/ui/Prestamos/PrestamosSolicitados/ConSolicitado.tsx
+++ src/ui/Prestamos/PrestamosSolicitados/ConSolicitado.tsx
@@ -1,46 +1,54 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { obtenerCuentas } from "../../../funcionesCliente/api/funcionesCuentas";
-
-
+import { obtenerPrestamosOtorgados, obtenerPrestamosSolicitados } from "../../../funcionesCliente/api/funcionesPrestamos";
+import { PrestamoSolicitado } from "../../../funcionesCliente/clases/prestamos/prestamoSolicitado";
+    
 export default function ConPresSol() {
     const nav = useNavigate();
-    const goHome = () => { nav('/menu_OtoPres') };
+    const goHome = () => { nav('/menu_SolPres') };
+    const prestamos = obtenerPrestamosSolicitados();
+    const [pres, setPres] = useState<PrestamoSolicitado>();
 
-    
+    if (prestamos.length === 0) { nav('/ErrorMensajeCuentas'); }
+
+    const showOption = ( e: { target: { value: any; }; } ) => {
+        let key = e.target.value;
+        let obj = prestamos[Number(key)];
+        setPres(obj);
+    }
     
     return (    
         <>
-        <div className="bg">
-        <div className="mainMod">
-            <h1>Prestamos Solicitados</h1>
-                <div id="mainP">
-                    Elige una Cuenta a Consultar:
-                    <br/>
-                    <select id="cuenta">
-                        <option value="null" >Seleccione una cuenta</option>
-                        <option value="1" >Mercantil, 3781739401283651, corriente, 20</option>
-                    </select>
-                    <br />
-                    <br />
-                    <p>NombreBanco: Mercantil</p>
-                    <p>NumeroCuenta: 3781739401283651</p>
-                    <p>Saldo: $20</p>
-                    <p>TipoCuenta: corriente</p>
-                    <br />
-                    <select id="prestamo">
-                        <option value="null" >Seleccione un prestamo solicitado</option>
-                        <option value="1" >Andres, 50</option>
-                    </select>
-                    <br />
-                    <p>Contacto: Andres</p>
-                    <p>Saldo: $50</p>
-                    <div id="card" className="card">
-                    </div>
+            <div className="bg">
+                <div className="mainMod">
+                    <h1>Consultar Prestamos Solicitados</h1>
+                        <div id="mainP">
+                            Elige un Prestamo a Consultar:
+                            <br/>
+                            <select id="cuenta" onChange={ showOption } >
+                                <option value="null" >Seleccione una opción</option>
+                                {prestamos.map((v, i) => {
+                                    return (
+                                        <option value={i} key={i}>{
+                                            v.acreedor.nombre + ", " +
+                                            v.cuenta.numCuenta + ", " +
+                                            v.valor + ", " +
+                                            v.fecha
+                                        }
+                                        </option>
+                                    )
+                                })}
+                            </select>
+                            <div id="card" className="card">
+                                <p>Nombre Acreedor: {pres?.acreedor.nombre}</p>
+                                <p>Numero de cuenta: {pres?.cuenta.numCuenta}</p>
+                                <p>Monto: ${pres?.valor}</p>
+                                <p>Fecha: {pres?.fecha}</p>
+                            </div>
+                        </div>
+                    <button onClick={ goHome } className="glow-button" >Regresar</button>
                 </div>
-            <button onClick={ goHome } className="glow-button" >Regresar</button>
-        </div>
-        </div>
+            </div>
         </>
     );
 }
